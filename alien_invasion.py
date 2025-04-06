ed6620db2ee0f554fe412d438e87e5ba8b53a853
Gene107 <ecook13@student.cscc.edu>
2025-04-06 09:55:42 -0400

    Added ability to fire ammo and having a limit to the amount, added laser sound

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from arsenal import Arsenal
+
 
 class AlienInvasion:
 
@@ -20,7 +22,11 @@ class AlienInvasion:
         self.running = True
         self.clock = pygame.time.Clock()
 
-        self.ship = Ship(self)
+        pygame.mixer.init()
+        self.laser_sound = pygame.mixer.Sound(self.settings.laser_sound)
+        self.laser_sound.set_volume(0.7)
+
+        self.ship = Ship(self, Arsenal(self))
 
     def run_game(self) -> None:
         # Game loop
@@ -46,7 +52,8 @@ class AlienInvasion:
                 self._check_keydown_events(event)
 
             elif event.type == pygame.KEYUP:
-                self._check_keyup_events(event) 
+                self._check_keyup_events(event)
+
 
 
     def _check_keyup_events(self, event) -> None:
@@ -60,6 +67,10 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            if self.ship.fire():
+                self.laser_sound.play()
+                self.laser_sound.fadeout(250)
         elif event.key == pygame.K_q:
             self.running = False
             pygame.quit()
