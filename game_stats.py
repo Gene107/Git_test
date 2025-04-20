7ca4b182d62516a760e8c3abb9ce12dc5b766378
Gene107 <ecook13@student.cscc.edu>
2025-04-20 15:21:04 -0400

    fleshed out gamestats to update hi score and save and read from a file

--- game_stats.py
+++ game_stats.py
@@ -2,6 +2,8 @@
 # April 13, 2025
 # Alien Invasion Game Stats
 
+from pathlib import Path
+import json
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from alien_invasion import AlienInvasion
@@ -12,7 +14,30 @@ class GameStats():
         self.game = game
         self.settings = game.settings
         self.max_score = 0
+        self.init_saved_scores()
         self.reset_stats()
+
+    def init_saved_scores(self):
+        self.path = self.settings.scores_file
+        if self.path.exists and self.path.stat.__sizeof__() > 80:
+            contents = self.path.read_text()
+            scores = json.loads(contents)
+            self.hi_score = scores.get('hi_score', 0)
+        else:
+            self.hi_score = 0
+            self.save_scores()
+            #save file
+
+    def save_scores(self)-> None:
+        scores = {
+            'hi_score': self.hi_score
+        }
+        contents = json.dumps(scores, indent=4)
+        try:
+            self.path.write_text(contents)
+        except FileNotFoundError as e:
+            print(f"File Not Found: {e.value}") 
+
         
     def reset_stats(self)-> None:
         self.ships_left = self.settings.starting_ship_count
